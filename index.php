74036d304324afe8de4b1f3648c841cdd7f40776
Adriano Oliveira <adrianooliveira2332@gmail.com>
2022-09-27 19:16:11 +0100

    added phpunit by composer and changed autoload to vendor/autoload

--- index.php
+++ index.php
@@ -2,15 +2,15 @@
     session_start();
     
     require "config.php";
-
-    spl_autoload_register(function ($class){
-        if(file_exists("controllers/".$class.".php"))
-            require "controllers/".$class.".php";
-        else if(file_exists("models/".$class.".php"))
-            require "models/".$class.".php";
-        else if(file_exists("core/".$class.".php"))
-            require "core/".$class.".php";
-    });
+    require "vendor/autoload.php";
+    // spl_autoload_register(function ($class){
+    //     if(file_exists("controllers/".$class.".php"))
+    //         require "controllers/".$class.".php";
+    //     else if(file_exists("models/".$class.".php"))
+    //         require "models/".$class.".php";
+    //     else if(file_exists("core/".$class.".php"))
+    //         require "core/".$class.".php";
+    // });
 
     $core = new Core();
     $core->run();
